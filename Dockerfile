3c124c71cef469672a8107aa7dac239915cfdcc4
James D <boboysdadda@gmail.com>
2020-04-25 07:46:17 -0600

    updated to actually work

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@ FROM python:3
 ADD requirements.txt /
 RUN pip install -r requirements.txt
 
-ADD kevin/kevin.py /
+ADD kevin/ /
 ADD kevin/command_list.yaml /
 
 
-CMD [ "python", "./kevin/kevin.py"]
+CMD [ "python", "kevin.py"]
--- kevin/kevin.py
+++ kevin/kevin.py
@@ -20,36 +20,36 @@ class Kevin(commands.Cog):
             option_list.append(key)
         return option_list
 
-    async def _get_response(self, parent_command, child_command):
-        await self.command_list.get(parent_command).get(child_command)
+    def _get_response(self, parent_command, child_command):
+        return self.command_list.get(parent_command).get(child_command)
 
     async def _help_option_wrapper(self, ctx):
         msg = ctx.message.content
-        option = msg[len(ctx.prefix) + 1 + len(ctx.invoked_with):]
+        option = msg[len(ctx.prefix) + len(ctx.invoked_with) + 1:]
         if option == '':
-            await ctx.send(f"{ctx.prefix} options:\n" + "\n".join(self._get_options(ctx.invoked_with)))
+            await ctx.send(f"{ctx.invoked_with} options:\n" + "\n".join(self._get_options(ctx.invoked_with)))
         else:
-            return ctx.send(self._get_response(ctx.prefix, option))
+            await ctx.send(self._get_response(ctx.invoked_with, option))
 
     @commands.command(aliases=['e'], help='!e option\nWithout an option will list options')
     @commands.has_permissions(embed_links=True)
     async def example(self, ctx):
-        return self._help_option_wrapper(ctx)
+        await self._help_option_wrapper(ctx)
 
     @commands.command(aliases=['d'], help='!d option\nWithout an option will list options')
     @commands.has_permissions(embed_links=True)
     async def docs(self, ctx):
-        return self._help_option_wrapper(ctx)
+        await self._help_option_wrapper(ctx)
 
     @commands.command(aliases=['t'], help='!t option\nWithout an option will list options')
     @commands.has_permissions(embed_links=True)
     async def tutorials(self, ctx):
-        return self._help_option_wrapper(ctx)
+        await self._help_option_wrapper(ctx)
 
     @commands.command(aliases=['m'], help='!m option\nWithout an option will list options')
     @commands.has_permissions(embed_links=True)
     async def macros(self, ctx):
-        return self._help_option_wrapper(ctx)
+        await self._help_option_wrapper(ctx)
 
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"),
