4a8cb92e26bd7e0223eb2a548c9de2a1ac5ef361
boboysdadda <boboysdadda@gmail.com>
2020-04-24 21:29:51 -0600

    Create help context (#1)
    * added help context
    
    * added help context

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 
 
 class Kevin(commands.Cog):
+
     def __init__(self, bot):
         self.bot = bot
         self.command_list = self.get_command_list()
@@ -18,21 +19,66 @@ class Kevin(commands.Cog):
     def get_response(self, parent_command, child_command):
         return self.command_list.get(parent_command).get(child_command)
 
-    @commands.command()
-    async def example(self, ctx, message):
-        await ctx.send(self.get_response("example", message))
+    def get_options(self, parent_command):
+        option_list = []
+        for key in self.command_list.get(parent_command):
+            option_list.append(key)
+        return option_list
 
-    @commands.command()
-    async def docs(self, ctx, message):
-        await ctx.send(self.get_response("docs", message))
+    @commands.command(aliases=['e'], help='!e option\nWithout an option will list options')
+    @commands.has_permissions(embed_links=True)
+    async def example(self, ctx):
+        # Next we get the message with the command in it.
+        msg = ctx.message.content
 
-    @commands.command()
-    async def tutorials(self, ctx, message):
-        await ctx.send(self.get_response("tutorials", message))
+        # Extracting the text sent by the user
+        # ctx.invoked_with gives the alias used
+        # ctx.prefix gives the prefix used while invoking the command
+        prefix_used = ctx.prefix
+        alias_used = ctx.invoked_with
+        text = msg[len(prefix_used) + len(alias_used):]
+        if text == '':
+            await ctx.send(f"Example Options:\n" + "\n".join(self.get_options("example")))
+        else:
+            await ctx.send(self.get_response("example", text))
 
-    @commands.command()
-    async def macros(self, ctx, message):
-        await ctx.send(self.get_response("macros", message))
+    @commands.command(aliases=['d'], help='!d option\nWithout an option will list options')
+    @commands.has_permissions(embed_links=True)
+    async def docs(self, ctx):
+        msg = ctx.message.content
+        prefix_used = ctx.prefix
+        alias_used = ctx.invoked_with
+        text = msg[len(prefix_used) + len(alias_used):]
+        if text == '':
+            await ctx.send(f"Docs Options:\n" + "\n".join(self.get_options("docs")))
+        else:
+
+            await ctx.send(self.get_response("docs", text))
+
+    @commands.command(aliases=['t'], help='!t option\nWithout an option will list options')
+    @commands.has_permissions(embed_links=True)
+    async def tutorials(self, ctx):
+        msg = ctx.message.content
+        prefix_used = ctx.prefix
+        alias_used = ctx.invoked_with
+        text = msg[len(prefix_used) + len(alias_used):]
+        if text == '':
+            await ctx.send(f"Tutorials Options:\n" + "\n".join(self.get_options("tutorials")))
+        else:
+            await ctx.send(self.get_response("tutorials", text))
+
+    @commands.command(aliases=['m'], help='!m option\nWithout an option will list options')
+    @commands.has_permissions(embed_links=True)
+    async def macros(self, ctx):
+
+        msg = ctx.message.content
+        prefix_used = ctx.prefix
+        alias_used = ctx.invoked_with
+        text = msg[len(prefix_used) + len(alias_used):]
+        if text == '':
+            await ctx.send(f"Macro Options:\n" + "\n".join(self.get_options("macros")))
+        else:
+            await ctx.send(self.get_response("macros", text))
 
 
 class GitBot(commands.Cog):
@@ -46,15 +92,13 @@ class GitBot(commands.Cog):
 
     @commands.command()
     async def list_open_issues(self, ctx):
-
         open_issues = self.repo.get_issues(state='open')
         for issue in open_issues:
             await ctx.send(issue)
 
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"),
-                   description='Relatively simple music bot example')
-
+                   description='Klipper Support Bot')
 bot.add_cog(Kevin(bot))
 # bot.add_cog(GitBot(bot))
 bot.run(environ.get('DISCORDTOKEN'))
