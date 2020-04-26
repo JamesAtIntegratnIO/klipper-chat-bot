fb2030788fe159bf9f876328c22b72d3e878fb4e
James D <boboysdadda@gmail.com>
2020-04-26 08:51:34 -0600

    added lookup typo handling to get the closest response

--- cogs/kevin.py
+++ cogs/kevin.py
@@ -1,6 +1,7 @@
 from os import environ
 from discord.ext import commands
 import yaml
+import difflib
 
 
 class Kevin(commands.Cog):
@@ -21,7 +22,16 @@ class Kevin(commands.Cog):
         return option_list
 
     def _get_response(self, parent_command, child_command):
-        return f'<{self.command_list.get(parent_command).get(child_command)}>'
+        response = self.command_list.get(parent_command).get(child_command)
+        if not response:
+            best_match = difflib.get_close_matches(child_command, self.command_list.get(parent_command))
+            print(f'best_match: {best_match}')
+            if not best_match:
+                return f'Could not anything like option: {child_command}'
+            else:
+                return f'<{self.command_list.get(parent_command).get(best_match[0])}>'
+        else:
+            return f'<{response}>'
 
     async def _help_option_wrapper(self, ctx):
         msg = ctx.message.content
