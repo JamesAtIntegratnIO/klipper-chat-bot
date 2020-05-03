20248755f8227f8d469bfbe9768da45bfc78f0c4
boboysdadda <boboysdadda@gmail.com>
2020-05-03 10:41:51 -0600

    Kevin now slips into your DMs (#10)

--- cogs/kevin.py
+++ cogs/kevin.py
@@ -1,7 +1,9 @@
 from os import environ
 from discord.ext import commands
+from discord import DMChannel
 import yaml
 import difflib
+import re
 
 
 class Kevin(commands.Cog):
@@ -66,36 +68,54 @@ class Kevin(commands.Cog):
         else:
             await ctx.send(self._get_response(self.get_command(ctx.invoked_with), option))
 
+    async def _dm_user_wrapper(self, ctx):
+        sep = '<'
+        message = ctx.message.content[len(ctx.prefix) + len(ctx.invoked_with) + 1:]
+        option = message.split(sep, 1)[0]
+        print(f'option: {option}')
+        if ctx.message.mentions:
+            user = ctx.guild.get_member(ctx.message.mentions[0].id)
+        else:
+            user = ctx.message.author
+        if option == '':
+
+            await DMChannel.send(user, f"```{ctx.invoked_with} options:\n" +
+                           "\n".join(self._get_options(self.get_command(ctx.invoked_with))) + "```")
+        else:
+            await DMChannel.send(user, self._get_response(self.get_command(ctx.invoked_with), option))
+
+
     @commands.command(help='!all option\n Will fetch all occurrences of option from all commands.  ')
     @commands.has_permissions(embed_links=True)
     async def all(self, ctx):
         msg = ctx.message.content
         option = msg[len(ctx.prefix) + len(ctx.invoked_with) + 1:]
+        user = ctx.message.author
         response = (list(self.get_related_links(self.command_list, option)))
         escaped_response = ["<" + resp + ">" for resp in response]
-        await ctx.send(f"\n\n".join(escaped_response))
+        await DMChannel.send(user, f"\n\n".join(escaped_response))
 
     @commands.command(aliases=['ex'], help='!examples option\nWithout an option will list options')
     @commands.has_permissions(embed_links=True)
     async def example(self, ctx):
-        await self._help_option_wrapper(ctx)
+        await self._dm_user_wrapper(ctx)
 
     @commands.command(aliases=['do'], help='!docs option\nWithout an option will list options')
     @commands.has_permissions(embed_links=True)
     async def doc(self, ctx):
-        await self._help_option_wrapper(ctx)
+        await self._dm_user_wrapper(ctx)
 
     @commands.command(aliases=['tu'], help='!tutorials option\nWithout an option will list options')
     @commands.has_permissions(embed_links=True)
     async def tutorial(self, ctx):
-        await self._help_option_wrapper(ctx)
+        await self._dm_user_wrapper(ctx)
 
     @commands.command(aliases=['ma'], help='!macros option\nWithout an option will list options')
     @commands.has_permissions(embed_links=True)
     async def macro(self, ctx):
-        await self._help_option_wrapper(ctx)
+        await self._dm_user_wrapper(ctx)
 
     @commands.command(aliases=['to'], help='!tools option\nWithout an option will list options')
     @commands.has_permissions(embed_links=True)
     async def tool(self, ctx):
-        await self._help_option_wrapper(ctx)
+        await self._dm_user_wrapper(ctx)
