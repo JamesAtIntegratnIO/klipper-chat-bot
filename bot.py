08f7668b4bfd6aadaf009b5775a72e3cdeb85822
James D <boboysdadda@gmail.com>
2020-05-03 11:06:48 -0600

    update description

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from cogs.info import Information
 if __name__ == '__main__':
 
     BOT = commands.Bot(command_prefix=commands.when_mentioned_or("!"),
-                       description='Klipper Support Bot')
+                       description="Klipper Support Bot\nI'll PM you some helpful links if you ask nicely")
     BOT.add_cog(Kevin(BOT))
     BOT.add_cog(GithubCog(BOT))
     BOT.add_cog(Information(BOT))
